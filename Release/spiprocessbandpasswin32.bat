ca49ebfb9590d448aa72ba5c821367c6ac9762ec
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 08:30:40 -0500

    first commit

---
+++ Release/spiprocessbandpasswin32.bat
@@ -0,0 +1,3 @@
+rem start spiprocessbandpasswin32.exe "E-MU ASIO" 0 1 "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+rem start spiprocessbandpasswin32.exe "E-MU ASIO" 4 5 "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+start spiprocessbandpasswin32.exe "E-MU ASIO" 6 7  "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
